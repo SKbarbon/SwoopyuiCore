0f9df87a88a28f802919edcd003d1bf022da7fd7
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-08-23 14:13:04 +0300

    u

---
+++ Sources/SwoopyuiCore/SayHi.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Yousif Aladwani on 23/08/2023.
+//
+
+import Foundation
+
+
+public struct SayHi {
+    public func sayHi () -> String {
+        return "Hola!"
+    }
+}
