1e76fe0b4cca9546433fc44bd141371d8f38cf63
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-08-27 14:03:35 +0300

    u

---
+++ Sources/SwoopyuiCore/subviews/TheVstackView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Yousif Aladwani on 27/08/2023.
+//
+
+import SwiftUI
+
+struct TheVstackView: View {
+    @Binding var subviewData : SubView
+    @Binding var subviewupdates : [SubViewUpdateRequest]
+    var body: some View {
+        VStack {
+            ForEach (subviewData.sub_views!, id: \.update_id) {subv in
+                ViewGenerator(subviewData: subv, hostUpdates: $subviewupdates)
+            }
+        }
+    }
+}
--- Sources/SwoopyuiCore/tools/ViewGenerator.swift
+++ Sources/SwoopyuiCore/tools/ViewGenerator.swift
@@ -14,10 +14,43 @@ struct ViewGenerator: View {
         VStack {
             if  subviewData.name == "Text" {
                 TheTextView(subviewData: $subviewData)
+            } else if subviewData.name == "VStack" {
+                TheVstackView (subviewData: $subviewData, subviewupdates: $hostUpdates)
+            }
+            
+            else {
+                Text("SwoopyUI have no view named \(subviewData.name!)")
+                    .foregroundStyle(.red)
             }
         }
         .onChange (of: hostUpdates){_ in
-            
+            if hostUpdates != [] {
+                if hostUpdates[0].action == "add_subview" {
+                    addNewSubview(updat:  hostUpdates[0])
+                    hostUpdates.remove(at: 0)
+                } else if hostUpdates[0].action == "update_subview" {
+                    updateSubviewProps(updat: hostUpdates[0])
+                    hostUpdates.remove(at: 0)
+                }
+            }
+        }
+    }
+    func updateSubviewProps (updat:SubViewUpdateRequest) {
+        if subviewData.sub_views != [] && subviewData.sub_views != nil {
+            var newSubviewsList : [SubView] = []
+            for sv in subviewData.sub_views! {
+                if sv.ID == updat.subview_data?.ID {
+                    newSubviewsList.append(updat.subview_data!)
+                } else {
+                    newSubviewsList.append(sv)
+                }
+            }
+            subviewData.sub_views = newSubviewsList
+        }
+    }
+    func addNewSubview (updat:SubViewUpdateRequest) {
+        if Int(updat.to_id!)! == subviewData.ID {
+            subviewData.sub_views?.append(updat.subview_data!)
         }
     }
 }
