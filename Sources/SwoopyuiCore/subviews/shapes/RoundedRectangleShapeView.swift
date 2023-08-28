fbeb31a60ff2b459d4ce5ca904f4985ced23ef6d
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-08-28 16:31:29 +0300

    u

--- Sources/SwoopyuiCore/subviews/shapes/RoundedRectangleShapeView.swift
+++ Sources/SwoopyuiCore/subviews/shapes/RoundedRectangleShapeView.swift
@@ -11,6 +11,13 @@ struct RoundedRectangleShapeView: View {
     @Binding var subviewData : SubView
     var body: some View {
         RoundedRectangle(cornerRadius: CGFloat(subviewData.props?.corner_radius ?? 0))
+            .foregroundStyle(getColorByString(color: subviewData.props?.color ?? "primery"))
+        if subviewData.props?.width != nil {
+            self.frame(width: CGFloat(subviewData.props?.width ?? 0))
+        }
+        if subviewData.props?.height != nil {
+            self.frame(height: CGFloat(subviewData.props?.height ?? 0))
+        }
     }
 }
 
