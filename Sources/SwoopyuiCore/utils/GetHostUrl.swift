14a3e4ec811deba4811772b1fd9a1021549effd4
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-08-29 19:27:11 +0300

    Update GetHostUrl.swift

--- Sources/SwoopyuiCore/utils/GetHostUrl.swift
+++ Sources/SwoopyuiCore/utils/GetHostUrl.swift
@@ -9,12 +9,19 @@ import Foundation
 
 
 func getHostUrl (port:Int, target:String) -> String {
+    var urlName = ""
+    if let variableValue = getenv("hostUrlName"), let value = String(validatingUTF8: variableValue) {
+        urlName = "\(value)"
+    } else {
+        urlName = "localhost"
+    }
+    
     if target == "target_function" {
-        return "http://localhost:\(port)/start_target_function"
+        return "http://\(urlName):\(port)/start_target_function"
     } else if target == "get_updates" {
-        return "http://localhost:\(port)/get_updates"
+        return "http://\(urlName):\(port)/get_updates"
     } else if target == "push_update" {
-        return "http://localhost:\(port)/push_update"
+        return "http://\(urlName):\(port)/push_update"
     }
     return "http://localhost:\(port)"
 }
